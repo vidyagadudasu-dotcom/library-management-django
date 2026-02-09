6aa17e2a70db0bbdbd5a376b9f564d4de5256803
Srividya <vidyagadudasu@gmail.com>
2026-02-09 17:53:26 +0530

    Update wsgi.py

--- library_management/wsgi.py
+++ library_management/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_management.settings')
+django.setup()
+
+# Auto-migrate on startup (temporary, only for deployment)
+from django.core.management import call_command
+call_command('migrate', interactive=False)
+call_command('collectstatic', interactive=False, verbosity=0)
 
 application = get_wsgi_application()
